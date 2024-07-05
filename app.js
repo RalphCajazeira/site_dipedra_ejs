da589cb8e564cf974ff253bb22380b275e4362a6
RalphCajazeira <ralphmtk@gmail.com>
2024-07-05 15:26:41 -0300

    0.2

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const indexRoutes = require("./routes/index");
 
+// Configuração da view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Configuração do diretório público
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRoutes);
+// Rotas
+app.use("/", require("./routes/index"));
 
+// Inicialização do servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
