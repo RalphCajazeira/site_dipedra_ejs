856dceb507d101ada03c64f639e767afb510b484
RalphCajazeira <ralphmtk@gmail.com>
2024-07-08 09:26:20 -0300

    0.3

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+// Rota para a página inicial
 router.get("/", (req, res) => {
   res.render("pages/index");
 });
 
+// Rota para a página Quem Somos
 router.get("/about", (req, res) => {
   res.render("pages/about");
 });
 
-router.get("/catalog", (req, res) => {
-  res.render("pages/catalog");
+// Rota para a página Mostruário
+router.get("/mostruario", (req, res) => {
+  res.render("pages/mostruario");
 });
 
 module.exports = router;
